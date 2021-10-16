101daa0576c04f297769ac53491eedee688f7a90
Juliano Vendramini Codognos <66084938+JulianoVendramini@users.noreply.github.com>
2021-10-16 13:09:06 -0300

    finished the challenge

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import { useEffect, useState } from 'react';
 
-import { MovieCard } from './components/MovieCard';
-
 import { SideBar } from './components/SideBar';
-// import { Content } from './components/Content';
+import { Content } from './components/Content';
+import { Header } from './components/Header';
 
 import { api } from './services/api';
 
@@ -17,7 +16,7 @@ export interface GenreResponseProps {
   title: string;
 }
 
-interface MovieProps {
+export interface MovieProps {
   imdbID: string;
   Title: string;
   Poster: string;
@@ -60,17 +59,8 @@ export function App() {
     <div style={{ display: 'flex', flexDirection: 'row' }}>
       <SideBar genres={genres} handleClickButton={handleClickButton} selectedGenreId={selectedGenreId} />
       <div className="container">
-        <header>
-          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-        </header>
-
-        <main>
-          <div className="movies-list">
-            {movies.map(movie => (
-              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-            ))}
-          </div>
-        </main>
+      <Header selectedGenre={selectedGenre}/>
+      <Content movies={movies}/>
       </div>
     </div>
   )
